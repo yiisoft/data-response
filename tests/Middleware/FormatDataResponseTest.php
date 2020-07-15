a122c9483ee7468bd113127c45cd4899f4947dd9
yiiliveext <yiiliveext@gmail.com>
2020-07-15 20:31:49 +0300

    Remove router dependency

--- tests/Middleware/FormatDataResponseTest.php
+++ tests/Middleware/FormatDataResponseTest.php
@@ -3,15 +3,11 @@
 namespace Yiisoft\Yii\Web\Tests\Data\Middleware;
 
 use Nyholm\Psr7\Factory\Psr17Factory;
-use Nyholm\Psr7\Response;
 use Nyholm\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Yiisoft\Http\Status;
-use Yiisoft\Router\Route;
 use Yiisoft\DataResponse\Formatter\JsonDataResponseFormatter;
 use Yiisoft\DataResponse\Middleware\FormatDataResponse;
 use Yiisoft\DataResponse\DataResponse;
@@ -23,48 +19,26 @@ class FormatDataResponseTest extends TestCase
         $request = new ServerRequest('GET', '/test');
         $factory = new Psr17Factory();
         $dataResponse = new DataResponse(['test' => 'test'], 200, '', $factory);
-        $route = Route::get(
-            '/test',
-            static function () use ($dataResponse) {
-                return $dataResponse;
-            },
-            $this->getContainer([FormatDataResponse::class => new FormatDataResponse(new JsonDataResponseFormatter())])
-        )->addMiddleware(FormatDataResponse::class);
-        $result = $route->process($request, $this->getRequestHandler());
+        $formatter = new FormatDataResponse(new JsonDataResponseFormatter());
+        $result = $formatter->process($request, $this->getRequestHandler($dataResponse));
         $result->getBody()->rewind();
 
         $this->assertSame('{"test":"test"}', $result->getBody()->getContents());
         $this->assertSame(['application/json'], $result->getHeader('Content-Type'));
     }
 
-    private function getContainer(array $instances): ContainerInterface
+    private function getRequestHandler(ResponseInterface $response): RequestHandlerInterface
     {
-        return new class($instances) implements ContainerInterface {
-            private array $instances;
+        return new class($response) implements RequestHandlerInterface {
+            private ResponseInterface $response;
 
-            public function __construct(array $instances)
+            public function __construct(ResponseInterface $response)
             {
-                $this->instances = $instances;
+                $this->response = $response;
             }
-
-            public function get($id)
-            {
-                return $this->instances[$id];
-            }
-
-            public function has($id)
-            {
-                return isset($this->instances[$id]);
-            }
-        };
-    }
-
-    private function getRequestHandler(): RequestHandlerInterface
-    {
-        return new class() implements RequestHandlerInterface {
             public function handle(ServerRequestInterface $request): ResponseInterface
             {
-                return new Response(Status::NOT_FOUND);
+                return $this->response;
             }
         };
     }
