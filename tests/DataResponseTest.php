4df1710ecd14509e9747dbce25f7aea7f52dd592
romkatsu <r@tsurkanu.ru>
2020-08-05 11:26:52 +0300

    add tests

--- tests/DataResponseTest.php
+++ tests/DataResponseTest.php
@@ -40,9 +40,23 @@ class DataResponseTest extends TestCase
     public function testSetResponseFormatter(): void
     {
         $factory = new Psr17Factory();
-        $dataResponse = new DataResponse('test', Status::OK, '', $factory);
+        $dataResponse = new DataResponse(['test'=>1], Status::OK, '', $factory);
+        $dataResponse = $dataResponse->withResponseFormatter(new JsonDataResponseFormatter());
+        $dataResponse->getBody()->rewind();
+
+        $this->assertTrue($dataResponse->hasResponseFormatter());
+        $this->assertSame('{"test":1}', $dataResponse->getBody()->getContents());
+        $this->assertSame(['application/json'], $dataResponse->getHeader('Content-Type'));
+    }
+
+    public function testSetEmptyResponseFormatter(): void
+    {
+        $factory = new Psr17Factory();
+        $dataResponse = new DataResponse(null, Status::OK, '', $factory);
         $dataResponse = $dataResponse->withResponseFormatter(new JsonDataResponseFormatter());
 
         $this->assertTrue($dataResponse->hasResponseFormatter());
+        $this->assertSame('', $dataResponse->getBody()->getContents());
+        $this->assertSame(['application/json'], $dataResponse->getHeader('Content-Type'));
     }
 }
