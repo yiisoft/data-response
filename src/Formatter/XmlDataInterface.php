cbb98df6e7eeb9d3c057577597c4e3561d5025e9
devanych <mail@devanych.ru>
2021-06-10 22:52:00 +0300

    Apply suggestions

--- src/Formatter/XmlDataInterface.php
+++ src/Formatter/XmlDataInterface.php
@@ -5,24 +5,25 @@ declare(strict_types=1);
 namespace Yiisoft\DataResponse\Formatter;
 
 /**
- * XmlFormatDataInterface provides methods for formatting objects {@see XmlDataResponseFormatter} into XML data.
+ * XmlFormatDataInterface provides methods for formatting objects {@see XmlDataResponseFormatter} as XML data.
  */
 interface XmlDataInterface
 {
     /**
-     * Returns a valid XML tag name {@see https://www.w3.org/TR/REC-xml/#NT-NameStartChar}.
+     * Returns a valid XML tag name {@see https://www.w3.org/TR/REC-xml/#NT-NameStartChar}
+     * to use when formatting an object as XML.
      *
      * @return string The XML tag name.
      */
     public function xmlTagName(): string;
 
     /**
-     * Returns a data to format in XML.
+     * Returns an array of data to format as XML.
      *
-     * The data can be any scalar values, instances of `XmlDataInterface` or `Traversable`,
+     * The data can be any scalar values, instances of `XmlDataInterface`,
      * and arrays of any nesting consisting of the above values.
      *
-     * @return array The data to format in XML.
+     * @return array The data to format as XML.
      */
     public function xmlData(): array;
 }
