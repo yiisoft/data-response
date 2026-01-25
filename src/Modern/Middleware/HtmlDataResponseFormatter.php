67e783fadd5644246d4160d913dd298f19891dd7
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-01-25 11:26:30 +0300

    revert formatters

--- src/Modern/Middleware/HtmlDataResponseFormatter.php
+++ src/Modern/Middleware/HtmlDataResponseFormatter.php
@@ -4,59 +4,15 @@ declare(strict_types=1);
 
 namespace Yiisoft\DataResponse\Modern\Middleware;
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\StreamFactoryInterface;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
-use RuntimeException;
-use Stringable;
-use Yiisoft\DataResponse\Modern\DataResponse;
-use Yiisoft\Http\Header;
-
-use function is_scalar;
-use function sprintf;
+use Yiisoft\DataResponse\Modern\Formatter\HtmlResponseFormatter;
 
 /**
  * Formats DataResponse as HTML.
  */
-final class HtmlDataResponseFormatter implements MiddlewareInterface
+final class HtmlDataResponseFormatter extends DataResponseFormatter
 {
-    /**
-     * @param string $contentType The Content-Type header for the response.
-     * @param string $encoding The encoding for the Content-Type header.
-     */
-    public function __construct(
-        private readonly StreamFactoryInterface $streamFactory,
-        private readonly string $contentType = 'text/html',
-        private readonly string $encoding = 'UTF-8',
-    ) {}
-
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    public function __construct(HtmlResponseFormatter $formatter = new HtmlResponseFormatter())
     {
-        $response = $handler->handle($request);
-        if (!$response instanceof DataResponse) {
-            return $response;
-        }
-
-        $data = $response->getData();
-        $response = $response->getResponse();
-
-        if (!is_scalar($data) && $data !== null && !$data instanceof Stringable) {
-            throw new RuntimeException(
-                sprintf(
-                    'Data must be either a scalar value, null, or a stringable object. %s given.',
-                    get_debug_type($data),
-                ),
-            );
-        }
-
-        if (!empty($data)) {
-            $response = $response->withBody(
-                $this->streamFactory->createStream((string) $data),
-            );
-        }
-
-        return $response->withHeader(Header::CONTENT_TYPE, "$this->contentType; charset=$this->encoding");
+        parent::__construct($formatter);
     }
 }
--- src/Modern/Middleware/JsonDataResponseFormatter.php
+++ src/Modern/Middleware/JsonDataResponseFormatter.php
@@ -4,56 +4,15 @@ declare(strict_types=1);
 
 namespace Yiisoft\DataResponse\Modern\Middleware;
 
-use JsonException;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\StreamFactoryInterface;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
-use Yiisoft\DataResponse\Modern\DataResponse;
-use Yiisoft\Http\Header;
-use Yiisoft\Json\Json;
-
-use const JSON_UNESCAPED_SLASHES;
-use const JSON_UNESCAPED_UNICODE;
+use Yiisoft\DataResponse\Modern\Formatter\JsonResponseFormatter;
 
 /**
  * Formats DataResponse as JSON.
  */
-final class JsonDataResponseFormatter implements MiddlewareInterface
+final class JsonDataResponseFormatter extends DataResponseFormatter
 {
-    /**
-     * @param string $contentType The Content-Type header for the response.
-     * @param string $encoding The encoding for the Content-Type header.
-     * @param int $options The encoding options. For more details please refer to
-     * {@link https://www.php.net/manual/en/function.json-encode.php}.
-     */
-    public function __construct(
-        private readonly StreamFactoryInterface $streamFactory,
-        private readonly string $contentType = 'application/json',
-        private readonly string $encoding = 'UTF-8',
-        private readonly int $options = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE,
-    ) {}
-
-    /**
-     * @throws JsonException
-     */
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    public function __construct(JsonResponseFormatter $formatter = new JsonResponseFormatter())
     {
-        $response = $handler->handle($request);
-        if (!$response instanceof DataResponse) {
-            return $response;
-        }
-
-        $data = $response->getData();
-        $response = $response->getResponse();
-
-        if ($data !== null) {
-            $response = $response->withBody(
-                $this->streamFactory->createStream(Json::encode($data, $this->options)),
-            );
-        }
-
-        return $response->withHeader(Header::CONTENT_TYPE, "$this->contentType; charset=$this->encoding");
+        parent::__construct($formatter);
     }
 }
