1bf531f1761e9d7c7cb5e5d631a83cccaadb9834
vjik <525501+vjik@users.noreply.github.com>
2026-02-04 08:43:30 +0000

    Apply PHP CS Fixer and Rector changes (CI)

--- src/Formatter/DataEncodingException.php
+++ src/Formatter/DataEncodingException.php
@@ -6,6 +6,4 @@ namespace Yiisoft\DataResponse\Formatter;
 
 use LogicException;
 
-final class DataEncodingException extends LogicException
-{
-}
+final class DataEncodingException extends LogicException {}
