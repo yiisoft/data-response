7e539a8cf86c1bfd61a3eb6feced253d59ef81a0
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-02-05 11:43:57 +0300

    fix

--- src/Middleware/DataResponseMiddleware.php
+++ src/Middleware/DataResponseMiddleware.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Yiisoft\DataResponse\Middleware;
 
+use Yiisoft\DataResponse\DataStream\DataStream;
+
 /**
  * Middleware that applies a custom formatter to {@see DataStream} responses
  * and sets appropriate response headers.
